88754a5e070dc88a61912ad964fc914ff65b7c50
kaVII-x <ka52jiguli@yandex.ru>
2023-05-17 12:45:26 +0400

    version 1.0

---
+++ src/main/java/sky/pro/employeebooktest/EmployeeBookTestApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.employeebooktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/employeebooktest/EmployeeBookTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.employeebooktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
